f3ddc3d3326478bc437a96fc40cdee298dd81c23
Akaam Shamerany <alpharaohh@gmail.com>
2026-07-03 17:56:59 -0700

    Remove stuff

--- services/api/src/services/deck-engine/module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from "@nestjs/common";
-import { DeckEngineService } from "./service";
-
-@Global()
-@Module({
-  providers: [DeckEngineService],
-  exports: [DeckEngineService],
-})
-export class DeckEngineModule {}
